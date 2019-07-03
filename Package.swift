c1048d3eab3295f835e0efa004340f10bd375c90
Helge Heß <devteam@zeezide.de>
2019-07-03 16:04:23 +0200

    Tie master to build to a SwiftWebUI revision
    0.1.4 is the one which works w/ Xcode 11b3 and Linux.

--- Package.swift
+++ Package.swift
@@ -3,19 +3,18 @@
 import PackageDescription
 
 let package = Package(
-    name: "AvocadoToast",
-    platforms: [
-       .macOS(.v10_15), .iOS(.v13)
-    ],
-    products: [
-        .executable(name: "AvocadoToast", targets: [ "AvocadoToast" ])
-    ],
-    dependencies: [
-        .package(url: "https://github.com/SwiftWebUI/SwiftWebUI.git",
-                 .branch("develop"))
-    ],
-    targets: [
-        .target(name: "AvocadoToast",
-                dependencies: [ "SwiftWebUI" ])
-    ]
+  name: "AvocadoToast",
+  platforms: [
+    .macOS(.v10_15), .iOS(.v13)
+  ],
+  products: [
+    .executable(name: "AvocadoToast", targets: [ "AvocadoToast" ])
+  ],
+  dependencies: [
+    .package(url: "https://github.com/SwiftWebUI/SwiftWebUI.git",
+             from: "0.1.4")
+  ],
+  targets: [
+    .target(name: "AvocadoToast", dependencies: [ "SwiftWebUI" ])
+  ]
 )
